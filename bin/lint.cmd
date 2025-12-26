04fb89d911449d7c693c0a1b95bfd9504f98f01e
3y3 <3y3@ya.ru>
2025-12-27 00:04:05 +0300

    fix: correct path resolution in Windows .cmd wrapper
    - Fix path to lint script when called from node_modules/.bin/
    - Use proper relative path resolution for npm wrapper context
    - Should fix incorrect path with double backslashes on Windows

--- bin/lint.cmd
+++ bin/lint.cmd
@@ -1,7 +1,12 @@
 @echo off
 setlocal
-set SCRIPT_DIR=%~dp0
-set SCRIPT_PATH=%SCRIPT_DIR%lint
+REM Get the directory where this .cmd file is located
+set CMD_DIR=%~dp0
+REM Resolve the path to the actual lint script
+REM npm creates wrapper in node_modules/.bin/, so we need to go up and find the package
+set SCRIPT_PATH=%CMD_DIR%..\@diplodoc\lint\bin\lint
+REM Normalize path separators
+set SCRIPT_PATH=%SCRIPT_PATH:\=/%
 REM Try to find bash in common locations or PATH
 if exist "C:\Program Files\Git\bin\bash.exe" (
     "C:\Program Files\Git\bin\bash.exe" "%SCRIPT_PATH%" %*
